82d15edcf68e8a61a189bd826cca30af4554bf7f
User <dsahanperera@gmail.com>
2019-05-17 07:05:48 +0530

    project

---
+++ fork-demo/src/main/java/com/sahan/demo/ForkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sahan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForkDemoApplication.class, args);
+	}
+
+}
